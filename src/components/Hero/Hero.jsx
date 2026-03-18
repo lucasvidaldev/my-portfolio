58b82808c6ddf9216d4a8d76efdccbfefcb6c1cd
Lucas Vidal <luccas.lima12@gmail.com>
2026-03-18 13:28:01 -0300

    Terminando primeiras funcionalidades do Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,31 +3,34 @@ import { gsap } from 'gsap'
 import './Hero.css'
 
 function Hero() {
-  const titleRef = useRef(null)
+  const trackRef = useRef(null)
 
-useEffect(() => {
-  if (!titleRef.current) return
+  useEffect(() => {
+    const el = trackRef.current
 
-  const letters = titleRef.current.querySelectorAll('span')
-
-  gsap.fromTo(
-    letters,
-    { y: 120, opacity: 0 },
-    { y: 0, opacity: 1, duration: 1, ease: 'power3.out', stagger: 0.05 }
-  )
-}, [])
-
-  const text = "Lucas Gonçalves"
+    gsap.to(el, {
+      xPercent: -50, // 🔥 movimento horizontal
+      duration: 65,  // 🔥 mais lento = mais premium
+      ease: "none",  // 🔥 essencial pro loop perfeito
+      repeat: -1
+    })
+  }, [])
 
   return (
     <section className="hero">
-      <h1 className="hero__title" ref={titleRef}>
-        {text.split("").map((letter, index) => (
-          <span key={index}>
-            {letter === " " ? "\u00A0" : letter}
-          </span>
-        ))}
+      <h1 className="hero__title">
+        <div className="hero__track" ref={trackRef}>
+          <span>Lucas Gonçalves</span>
+          <span>Lucas Gonçalves</span>
+          <span>Lucas Gonçalves</span>
+          <span>Lucas Gonçalves</span>
+        </div>
       </h1>
+
+      <div className="hero__footer">
+         <span>São Roque - SP, Brasil</span>
+         <span className="hero__footer__dev">Junior Developer</span>
+      </div>
     </section>
   )
 }
