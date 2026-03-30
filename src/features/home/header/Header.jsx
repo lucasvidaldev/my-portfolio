6277beef8ef21e8e81f333096ef98c2b33fe8b04
Lucas Vidal <luccas.lima12@gmail.com>
2026-03-30 10:55:48 -0300

    Corrigindo problema de nav overlay

--- src/features/home/header/Header.jsx
+++ src/features/home/header/Header.jsx
@@ -4,7 +4,7 @@ import MobileMenu from './components/MobileMenu.jsx';
 
 export default function Header() {
   return (
-    <header className={`${styles.header} reveal`}>
+    <header className={styles.header}>
       <span className={styles.headerLogo}>lucas gonçalves</span>
       <nav>
         <NavMenu />
