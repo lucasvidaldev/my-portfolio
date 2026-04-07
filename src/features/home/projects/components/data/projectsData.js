cb04662350f0edc94d048a28381a1d437ceba64e
Lucas Vidal <luccas.lima12@gmail.com>
2026-04-07 16:45:22 -0300

    Adicionando informações e links do novo projeto

--- src/features/home/projects/components/data/projectsData.js
+++ src/features/home/projects/components/data/projectsData.js
@@ -1,27 +1,29 @@
  import Img1 from '../../../../../assets/images/projects/img1.jpg';
+ import ImgNexus from '../../../../../assets/images/projects/nexusbjj.png'
 
 
  export const projects = [
      {
-       id: 'veepee',
-       img: Img1,
-       eyebrow: 'France - Global - 2024-2026',
-       title: 'Veepee - Sistema de Design',
+       id: 'nexus',
+       img: ImgNexus,
+       eyebrow: 'São Roque - São Paulo - 2026',
+       title: 'Nexus - BJJ Academy',
        bullets: [
-         'Reestruturei layout com foco em UX/UI',
+         'Layout construido do Zero, com foco em UX/UI',
          'Melhorei navegação e consistência visual',
-         'Interface moderna baseada no Bikecraft',
+         'Interface moderna baseada no em projetos do Awwwards',
        ],
        techs: [
-         'HTML5',
-         'CSS3',
+         'React',
          'JavaScript',
+         'HTML5',
+         'CSS Modules',
+         'Responsivo Design',
          'Figma',
-         'UX/UI',
-         'Responsivo',
+         'UX/UI Design',
        ],
-       deploy: 'https://mybikestar.netlify.app/',
-       github: 'https://github.com/eulucasvidal/bikestar',
+       deploy: 'https://nexusbjjacademy.netlify.app/',
+       github: 'https://github.com/jobslucasvidal-design/nexusbjjacademy',
      },
      {
        id: 'kira',
